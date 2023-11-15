35400d970ee139831e7f77940ed6f760e27a7952
mikael-ayala <mikael.ayala.santos@gmail.com>
2023-11-15 18:42:39 -0300

    finish footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,10 +1,21 @@
 import './Footer.scss'
+import logoFooter from '../../assets/logo-footer.png'
+import Instagram from '../../assets/instagram.svg?react'
+import Whatsapp from '../../assets/whatsapp.svg?react'
 
 export default function Footer() {
     return (
         <footer className='footer'>
-            <div className="container">
-                
+            <div className="footer-container container">
+                <img className='logo-footer' src={logoFooter} />
+                <div className='icons-footer'>
+                    <a href="https://web.whatsapp.com/" target='_blank' rel="noreferrer">
+                        <Whatsapp width="24" height="24" fill="#fff" />
+                    </a>
+                    <a href="https://www.instagram.com/" target='_blank' rel="noreferrer">
+                        <Instagram width="24" height="24" fill="#fff" />
+                    </a>
+                </div>
             </div>
         </footer>
     )
