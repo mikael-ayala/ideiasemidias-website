c3c5c64f4a146e8afdd53d6ac5ee6393d66f8668
mikael-ayala <mikael.ayala.santos@gmail.com>
2023-11-28 13:34:04 -0300

    remove modal

--- src/components/PortfolioCard/index.jsx
+++ src/components/PortfolioCard/index.jsx
@@ -1,33 +1,14 @@
 import './PortfolioCard.scss'
-import { useState } from 'react';
-import PortfolioModal from '../PortfolioModal'
 
 export default function PortfolioCard({image, name, pages, format}) {
-    const [showModal, setShowModal] = useState(false)
-
-    function openModal() {
-        setShowModal(true)
-        document.body.style.overflow = 'hidden'
-    }
-
     return (
-        <>
-            <div className="portfolio-card" onClick={() => openModal()}>
-                <img className='img' src={image} />
-                <div className='text'>
-                    <p className='name'>{name}</p>
-                    <p className='description'>{pages}</p>
-                    <p className='description'>{format}</p>
-                </div>
+        <div className="portfolio-card">
+            <img className='img' src={image} />
+            <div className='text'>
+                <p className='name'>{name}</p>
+                <p className='description'>{pages}</p>
+                <p className='description'>{format}</p>
             </div>
-            <PortfolioModal
-                image={image}
-                name={name}
-                pages={pages}
-                format={format}
-                showModal={showModal}
-                setShowModal={setShowModal}
-            />
-        </>
+        </div>
     )
 }
